20ce80db3db2ec3ae9c0af5b796621545ac85ba8
hyenaManager <zodiacjelly9@gmail.com>
2024-01-31 21:58:26 +0630

    modify: bg-color

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -4,9 +4,7 @@ import SmSideBar from "@/components/smNavigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      className={`${lusitana.className} flex flex-col md:flex-row bg-[#D9A404]`}
-    >
+    <div className={`${lusitana.className} flex flex-col md:flex-row `}>
       <AsideNavbar />
       <SmSideBar />
       {children}
